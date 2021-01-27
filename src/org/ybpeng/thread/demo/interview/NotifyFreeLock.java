3cfab25baf2dff9314681579fd5842fbd45bee27
ybpeng <pengyabin1@newhope.cn>
2021-01-27 20:00:13 +0800

    lock

--- src/org/ybpeng/thread/demo/interview/NotifyFreeLock.java
+++ src/org/ybpeng/thread/demo/interview/NotifyFreeLock.java
@@ -8,43 +8,59 @@ import java.util.List;
  * 实现一个容器，提供两个方法，add，size
  * 写两个线程，线程1添加10个元素到容器中，线程2实现监控元素的个数，当个数到5个时，线程2给出提示并结束
  */
-
-
 public class NotifyFreeLock {
 
 
-    private List list = new ArrayList<String>();
-
-    int size(){
-        return list.size();
-    }
+    private List<String> list = new ArrayList<String>();
 
     void add(String str){
         list.add(str);
     }
 
+    int getSize(){
+        return list.size();
+    }
 
     public static void main(String[] args) {
 
-        NotifyFreeLock lock = new NotifyFreeLock();
 
-        Thread t2 = new Thread(()-> {
-            if (lock.size() == 5){
-                //线程2结束
-                System.out.println("T2 end here");
+        NotifyFreeLock obj = new NotifyFreeLock();
+
+        Object lock = new Object();
+
+
+        //先启动T2
+        Thread t2 = new Thread(() ->{
+            synchronized (lock){
+                if (obj.getSize() != 5){
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println("T2 thread end here...");
             }
         });
 
 
-        /*
-        * 线程1往list列表add元素
-        * */
-        Thread t1 = new Thread(()-> {
-            for (int i = 1;i<=10;i++){
-                lock.add(Integer.toString(i));
+        Thread t1 = new Thread(() -> {
+            for (int i = 1; i <= 10; i++) {
+                synchronized (lock) {
+                    obj.add(Integer.toString(i));
+                    System.out.println("T1 thread,list size = " + obj.getSize());
+                    if (obj.getSize() == 5) {
+                        //如果size等于5，则通知T2结束
+                        System.out.println("T1 thread notify T1 ");
+                        lock.notify();
+                    }
+                }
             }
         });
 
+        t2.start();
+
+        t1.start();
 
     }
 }
